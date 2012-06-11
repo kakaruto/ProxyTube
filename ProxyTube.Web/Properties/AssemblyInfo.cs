2c9d1f7f1afe7690c349460b14c83284eb8c1cd5
Philippe GARRY <philippe.garry@berger.levrault.fr>
2012-06-11 17:14:26 +0200

    Update MVC to Basic Project

--- ProxyTube.Web/Properties/AssemblyInfo.cs
+++ ProxyTube.Web/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ed6fee3d-b25e-4b4e-b811-691d0935a6f2")]
+[assembly: Guid("3c193f33-c1fd-49ff-9a6e-db19a2d0ba28")]
 
 // Version information for an assembly consists of the following four values:
 //
