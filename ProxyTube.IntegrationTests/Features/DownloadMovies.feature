723f2835b0e1e1ec20f3a80c2d8f017ef615ad29
Philippe GARRY <philippe.garry@berger.levrault.fr>
2012-06-11 17:45:14 +0200

    Add Skeleton of Integration Tests

---
+++ ProxyTube.IntegrationTests/Features/DownloadMovies.feature
@@ -0,0 +1,10 @@
+﻿Feature: Download
+	In order to watch youtube video at my work
+	As a user
+	I want to download youtube videos
+
+@mytag
+Scenario: Add youtube video to download
+	Given I have entered a youtube id
+	When I press add
+	Then the result should be downloading youtube id on my screen
---
+++ ProxyTube.IntegrationTests/Steps/ProxyTubeStepDefinitions.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace ProxyTube.IntegrationTests.Steps
+{
+    [Binding]
+    public class ProxyTubeStepDefinitions
+    {
+        [Given(@"I have entered a youtube id")]
+        public void GivenIHaveEnteredAYoutubeId()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result should be downloading youtube id on my screen")]
+        public void ThenTheResultShouldBeDownloadingYoutubeIdOnMyScreen()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
